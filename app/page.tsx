c3737a35f7303133d8214bb542d05a0972dedbcf
HM <71471857+haeytha@users.noreply.github.com>
2024-10-12 02:29:32 +0000

    Issue Link Commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,10 @@ import Nav from "./components/nav";
 
 export default function Page() {
     return <>
-
         <div className="container py-4">
             <div className="row">
                 <Header />
                 <Nav />
-
                 <div className="col-9">
                     <img className="d-block w-100 mb-3" src="https://fastly.picsum.photos/id/251/1280/720.jpg?hmac=YDyCcHsQDDzvzCfZ-MLi2fr2pFqpoH9OqSqmJFBwV7w" />
                     <h1>Welcome</h1>
